c1adbd488330e3ad3a6151e6fb9e2654e3b6fee6
GeethOnion <geethmaka@gmail.com>
2020-10-08 22:07:43 +0530

    Changed to a new time API

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # EST Time Extention
 
+Powered By - http://worldclockapi.com/api/json/est/now
 
 "Did anyone ask for this? No."
 -notnavindu
--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   function setTime(){
-    fetch("https://worldtimeapi.org/api/timezone/EST").then((response)=>response.json()).then(function(data){
-      var date=data.datetime.split("T")[0];
-      var time=data.datetime.split("T")[1].split(".");
-      document.getElementById("date").innerHTML=`${date}`;
+    fetch("http://worldclockapi.com/api/json/est/now").then((response)=>response.json()).then(function(data){
+      var date=data.currentDateTime.split("T");
+      var time=data.currentDateTime.split("T")[1].split("-");
+      document.getElementById("date").innerHTML=`${date[0]}`;
       document.getElementById("time").innerHTML=`${time[0]}`;
     })
   }
