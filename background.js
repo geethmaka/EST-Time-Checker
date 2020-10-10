f1525eef6f341b2c2bfc97b92473181f0af75868
Navindu <navanavi3@gmail.com>
2020-10-10 21:09:11 +0530

    feat: added loading indicator:

--- background.js
+++ background.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  function setTime(){
-    fetch("http://worldclockapi.com/api/json/est/now").then((response)=>response.json()).then(function(data){
-      var date=data.currentDateTime.split("T");
-      var time=data.currentDateTime.split("T")[1].split("-");
-      document.getElementById("date").innerHTML=`${date[0]}`;
-      document.getElementById("time").innerHTML=`${time[0]}`;
-    })
+  function setTime() {
+    fetch("http://worldclockapi.com/api/json/est/now")
+      .then((response) => response.json())
+      .then(function (data) {
+        console.log(data);
+        var date = data.currentDateTime.split("T");
+        var time = data.currentDateTime.split("T")[1].split("-");
+        document.getElementById("date").innerHTML = `${date[0]}`;
+        document.getElementById("time").innerHTML = `${time[0]}`;
+      });
   }
-  setTime()
-  var timer = setInterval(setTime,1000);
+  setTime();
+  var timer = setInterval(setTime, 1000);
 });
